285a1a2d7988a8a52b525a52e121863279c54601
Maykonx23 <802.140@alunos.unigran.br>
2022-06-25 12:48:12 -0400

    fix: Atualização Api

--- ormconfig.js
+++ ormconfig.js
@@ -3,6 +3,8 @@ module.exports = {
     url: process.env.DATABASE_URL,
     entities: ["dist/modules/**/typeorm/entities/*.js"],
     migrations: ["dist/shared/typeorm/migrations/*.js"],
+    ssl: "true",
+    extra: { ssl: { rejectUnauthorized: "false" } },
     cli: {
         migrationsDir: "./src/shared/typeorm/migrations",
     },
